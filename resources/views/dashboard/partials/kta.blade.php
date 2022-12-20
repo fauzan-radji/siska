2dc2eaf819fc6066f7305b2e361fd5ffa2ac4d41
fauzan-radji <triputrafauzan@gmail.com>
2022-12-21 03:55:25 +0800

    KARTU ANGGOTA: Dynamic, Downloadable

--- resources/views/dashboard/partials/kta.blade.php
+++ resources/views/dashboard/partials/kta.blade.php
@@ -1,17 +1,20 @@
-<div class="kartu-anggota">
+<div class="kartu-anggota" id="kta-{{ $id ?? '' }}">
   <div class="kartu-anggota__header">
-    <img src="/img/pramuka.png">
+    <div class="kartu-anggota__header-image" style="--url: url('/img/pramuka.png')"></div>
+    {{-- <img src="/img/pramuka.png"> --}}
     <div class="kartu-anggota__title text-center">
       <h2>KARTU TANDA ANGGOTA</h2>
       <h1>GERAKAN PRAMUKA</h1>
       <hr>
     </div>
-    <img src="/img/kota-gorontalo.png">
+    <div class="kartu-anggota__header-image" style="--url: url('/img/kota-gorontalo.png')"></div>
+    {{-- <img src="/img/kota-gorontalo.png"> --}}
   </div>
 
   <div class="kartu-anggota__body">
     <div class="kartu-anggota__side">
-      <img src="{{ $foto }}">
+      {{-- <img src="{{ $foto ?? '' }}"> --}}
+      <div class="kartu-anggota__side-image" style="--url: url('{{ $foto ?? '' }}')"></div>
     </div>
     <div class="kartu-anggota__main">
       <table>
@@ -19,42 +22,42 @@
           <td>No.&nbsp;Tanda&nbsp;Anggota</td>
           <td>:</td>
           <!-- <td colspan="4">05.02.00-000.008</td> -->
-          <td colspan="4">{{ $no_anggota }}</td>
+          <td colspan="4">{{ $no_anggota ?? '' }}</td>
         </tr>
         <tr>
           <td>Nama Lengkap</td>
           <td>:</td>
-          <td colspan="4">{{ $nama }}</td>
+          <td colspan="4">{{ $nama ?? '' }}</td>
         </tr>
         <tr>
           <td>Tempat&nbsp;Tgl.&nbsp;Lahir</td>
           <td>:</td>
           <!-- <td colspan="4">Konoha, 10 Oktober 1998</td> -->
-          <td colspan="4">{{ $tempat_lahir }}, {{ \Carbon\Carbon::parse($tanggal_lahir)->format('d-m-Y') }}</td>
+          <td colspan="4">{{ $tempat_lahir ?? '' }}, {{ \Carbon\Carbon::parse($tanggal_lahir ?? '01-01-2001')->format('d-m-Y') }}</td>
         </tr>
         <tr>
           <td>Alamat</td>
           <td>:</td>
-          <td colspan="4">{{ $alamat }}</td>
+          <td colspan="4">{{ $alamat ?? '' }}</td>
         </tr>
         <tr>
           <td>No. Telepon</td>
           <td>:</td>
-          <td colspan="4">{{ $no_hp }}</td>
+          <td colspan="4">{{ $no_hp ?? '' }}</td>
         </tr>
         <tr>
           <td>Agama</td>
           <td>:</td>
-          <td>{{ $agama }}</td>
+          <td>{{ $agama ?? '' }}</td>
           <td class="text-end">Gol.&nbsp;Darah</td>
           <td>:</td>
-          <td>{{ $gol_darah }}</td>
+          <td>{{ $gol_darah ?? '' }}</td>
         </tr>
         <tr>
           <td>Gol/Jabt</td>
           <td>:</td>
           <!-- <td colspan="4">Penegak Bantara/Pinru</td> -->
-          <td colspan="4">{{ $golongan }}/{{ $jabatan }}</td>
+          <td colspan="4">{{ $golongan ?? '' }}/{{ $jabatan ?? '' }}</td>
         </tr>
         <tr>
           <td>Kwarcab</td>
@@ -62,7 +65,7 @@
           <td>{!! $kwarcab !!}</td>
           <td class="text-end">Kwarda</td>
           <td>:</td>
-          <td>{{ $kwarda }}</td>
+          <td>{{ $kwarda ?? '' }}</td>
         </tr>
         <tr>
           <td></td>
@@ -79,11 +82,11 @@
               </tr>
               <tr>
                 <!-- <td class="kartu-anggota__nama-ketua">Fadel Djibran SE., MSi.</td> -->
-                <td class="kartu-anggota__nama-ketua">{{ $nama_ketua }}</td>
+                <td class="kartu-anggota__nama-ketua">{{ $nama_ketua ?? '' }}</td>
               </tr>
               <tr>
                 <!-- <td>NTA: 05.02.00-000.008</td> -->
-                <td>NTA: {{ $nta_ketua }}</td>
+                <td>NTA: {{ $nta_ketua ?? '' }}</td>
               </tr>
             </table>
           </td>
